ed38dc09b1bd0f721d44b0eec26ae64d2b13d851
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-05-05 18:19:38 +0600

    requireAuth some update

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
+
+
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     const [service, setService] = useState({});
